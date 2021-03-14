82633877a670fc5b188ad486de10540e7d98d4f1
EthanJF <ethanjosephfournier@gmail.com>
2021-03-14 14:22:07 -0400

    finished productOfArray and contains

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -1,7 +1,17 @@
-function productOfArray(numArray){
-    if(numArray.length === 1){
+function productOfArray(numArray) {
+    if (numArray.length === 1) {
         return numArray[0]
     } else {
-        return numArray[0] * productOfArray(numArray.shift())
+        return numArray.shift() * productOfArray(numArray)
+    }
+}
+
+function contains(nestedObj, searchTerm) {
+    if (Object.values(nestedObj)[0] === searchTerm) {
+        return true
+    } else if (typeof Object.values(nestedObj)[0] === "object") {
+        return contains(Object.values(nestedObj)[0], searchTerm)
+    } else {
+        return false
     }
 }

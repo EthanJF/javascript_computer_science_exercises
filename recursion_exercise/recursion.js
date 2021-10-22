004b7660f4983685605274b3aaf86b328e617127
EthanJF <ethanjosephfournier@gmail.com>
2021-10-22 17:47:18 -0400

    recursion

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -15,3 +15,45 @@ function contains(nestedObj, searchTerm) {
         return false
     }
 }
+
+function search(array, value){
+    let arrayCopy = [...array]
+    let i = 0
+    function helperSearch(arr, index){
+        if(arr[index] === value){
+            return index
+        } else if(index < arr.length) {
+            index++
+            return helperSearch(arr, index)
+        } else {
+            return -1
+        }
+    }
+    return helperSearch(arrayCopy, i)
+}
+
+function binarySearch(array, value){
+    let arrayToObject = {}
+    for(let i = 0; i < array.length; i++){
+        arrayToObject[array[i]] = i
+    }
+    let sortedArrayCopy = [...array].sort()
+    function helpSearch(array){
+        if(array.length === 1 && array[0] === value){
+            return true
+        } else if (array.length === 1 && array[0] !== value){
+            return false
+        }
+        else if(array[Math.ceil(array.length/2)] >= value){
+            helpSearch(array.slice(0, Math.ceil(array.length / 2)))
+        } else {
+            helpSearch(array.slice(Math.ceil(array.length / 2), array.length))
+        }
+    }
+
+    if(helpSearch(sortedArrayCopy)){
+        return arrayToObject[value]
+    } else {
+        return -1
+    }
+}
